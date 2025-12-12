7a553acb5a79357692b4a484c8d3dec27c4a7d29
TJA214 <agustint@sheridancollege.ca>
2025-12-11 23:48:14 -0500

    game.java

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -8,20 +8,23 @@ package ca.sheridancollege.project;
 import java.util.ArrayList;
 
 /**
- * The class that models your game. You should create a more specific child of this class and instantiate the methods
- * given.
+ * The class that models your game. You should create a more specific child of
+ * this class and instantiate the methods given.
  *
  * @author dancye
  * @author Paul Bonenfant Jan 2020
  */
 public abstract class Game {
 
-    private final String name;//the title of the game
-    private ArrayList<Player> players;// the players of the game
+    // the title of the game
+    private final String name;
+
+    // the players of the game
+    private ArrayList<Player> players;
 
     public Game(String name) {
         this.name = name;
-        players = new ArrayList();
+        players = new ArrayList<Player>();
     }
 
     /**
@@ -46,13 +49,15 @@ public abstract class Game {
     }
 
     /**
-     * Play the game. This might be one method or many method calls depending on your game.
+     * Play the game. This might be one method or many method calls depending on
+     * your game.
      */
     public abstract void play();
 
     /**
-     * When the game is over, use this method to declare and display a winning player.
+     * When the game is over, use this method to declare and display a winning
+     * player.
      */
     public abstract void declareWinner();
 
-}//end class
+} // end class
